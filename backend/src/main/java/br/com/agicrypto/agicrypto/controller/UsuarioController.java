1d2b0dc345403db8e030c46cfaf175122c69c44d
Ana Julia de C Leandro <afloradda@gmail.com>
2025-09-18 11:48:09 -0300

    feat: Implementando uso do service + nova rota via controller

--- backend/src/main/java/br/com/agicrypto/agicrypto/controller/UsuarioController.java
+++ backend/src/main/java/br/com/agicrypto/agicrypto/controller/UsuarioController.java
@@ -2,7 +2,10 @@ package br.com.agicrypto.agicrypto.controller;
 
 import br.com.agicrypto.agicrypto.model.Usuarios;
 import br.com.agicrypto.agicrypto.repository.UsuarioRepository;
+import br.com.agicrypto.agicrypto.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +15,18 @@ import java.util.List;
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    private final UsuarioRepository repository;
+    private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioRepository repository) { this.repository = repository; }
+    public UsuarioController(UsuarioService usuarioService) { this.usuarioService = usuarioService; }
 
     @GetMapping("/listar")
-    public List<Usuarios> listar() { return  repository.findAll(); }
+    public List<Usuarios> listar() { return usuarioService.listarTodos(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuarios> buscarPorId(@PathVariable Integer id) {
+        return usuarioService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
--- backend/src/main/java/br/com/agicrypto/agicrypto/service/UsuarioService.java
+++ backend/src/main/java/br/com/agicrypto/agicrypto/service/UsuarioService.java
@@ -1,4 +1,25 @@
 package br.com.agicrypto.agicrypto.service;
 
+import br.com.agicrypto.agicrypto.model.Usuarios;
+import br.com.agicrypto.agicrypto.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService (UsuarioRepository usuarioRepository) { this.usuarioRepository = usuarioRepository; }
+
+    public List<Usuarios> listarTodos() { return usuarioRepository.findAll(); }
+
+    public Optional<Usuarios> buscarPorId(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
 }
