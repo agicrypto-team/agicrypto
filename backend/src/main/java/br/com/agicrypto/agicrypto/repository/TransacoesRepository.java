780f424cc9ee94354952c61353284ea539447b20
Ana Julia de C Leandro <afloradda@gmail.com>
2025-09-18 08:18:32 -0300

    refactor: mudando arquivos de diretório (do service p/ repository)

--- backend/src/main/java/br/com/agicrypto/agicrypto/repository/TransacoesRepository.java
+++ backend/src/main/java/br/com/agicrypto/agicrypto/repository/TransacoesRepository.java
@@ -1,4 +1,4 @@
-package br.com.agicrypto.agicrypto.service;
+package br.com.agicrypto.agicrypto.repository;
 
 import br.com.agicrypto.agicrypto.model.Transacoes;
 import org.springframework.data.jpa.repository.JpaRepository;
