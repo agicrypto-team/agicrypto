543317d4d2eb10ffc21f23d0e9a212c958f26f80
Ana Julia de C Leandro <afloradda@gmail.com>
2025-09-30 08:35:20 -0300

    refactor: corrigindo nome referente ao arquivo Application ('Agricripto' para 'Agicripto')

--- backend/agricripto/src/main/java/com/devsdoagi/agicripto/AgicriptoApplication.java
+++ backend/agricripto/src/main/java/com/devsdoagi/agicripto/AgicriptoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgricriptoApplication {
+public class AgicriptoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgricriptoApplication.class, args);
+		SpringApplication.run(AgicriptoApplication.class, args);
 	}
 
 }
